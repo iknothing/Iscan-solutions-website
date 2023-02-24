18e76072535dfe33b4457d0452af9c4102b6f275
manasc <78652689+iknothing@users.noreply.github.com>
2023-02-24 19:46:59 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   
       services.each(function(i, service) {
         var img = $(service).find('.service-image');
-        var delay = i * 500;
+        var delay = 0;
   
         setTimeout(function() {
           img.toggleClass('show');
@@ -12,4 +12,66 @@ $(document).ready(function() {
       });
     });
   });
+  
+
+  $(document).ready(function() {
+    $('#show-images1').on('click', function() {
+      var services = $('.service');
+  
+      services.each(function(i, service) {
+        var img = $(service).find('.service-image1');
+        var delay = 0;
+  
+        setTimeout(function() {
+          img.toggleClass('show');
+        }, delay);
+      });
+    });
+  });
+
+  $(document).ready(function() {
+    $('#show-images2').on('click', function() {
+      var services = $('.service');
+  
+      services.each(function(i, service) {
+        var img = $(service).find('.service-image2');
+        var delay = 0;
+  
+        setTimeout(function() {
+          img.toggleClass('show');
+        }, delay);
+      });
+    });
+  });
+  
+//   $(document).ready(function() {
+//   $('#show-images1').on('click', function() {
+//     var services = $('.service');
+//     var delay = 0;
+  
+//     services.each(function(i, service) {
+//       var img = $(service).find('.service-image1');
+//       delay += 100;
+  
+//       setTimeout(function() {
+//         img.animate({
+//           opacity: 'toggle',
+//           height: 'toggle'
+//         }, 500);
+//       }, delay);
+//     });
+//   });
+// });
+
+// var btn = document.querySelector('#show-images');
+// var services = document.querySelectorAll('.service');
+// var images = document.querySelectorAll('.service-image');
+
+// btn.addEventListener('click', function() {
+//   for (var i = 0; i < services.length; i++) {
+//     images[i].classList.toggle('show');
+//   }
+// });
+
+
   
